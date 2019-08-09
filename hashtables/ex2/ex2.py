244d2ca1183dc9134e82655990fc6668607b35a0
“Josefia <josefia.magagnoli@gmail.com>
2019-08-09 12:49:10 -0400

    Example 2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,17 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    for ticket in tickets:
+        if ticket.source == "NONE":
+            hash_table_insert(hashtable, 'start', ticket.destination)
+        else:
+            hash_table_insert(hashtable, ticket.source, ticket.destination)
+            # We can hash each ticket such that the starting 
+            # location is the key and the destination is the value.
+            # Then, when constructing the entire route, 
+            # the `i`th location in the route can be found by checking
+            #  the hash table for the `i-1`th location.
+    route[0] =hash_table_retrieve(hashtable, 'start')
+    for i in range (1, length):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
     return route
